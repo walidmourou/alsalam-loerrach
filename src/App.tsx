bb7f44a393c6122ac3933f0e9c7ae845ca999dd5
Walid Mourou <46028071+walidmourou@users.noreply.github.com>
2025-04-04 23:42:20 +0200

    adapt header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -99,16 +99,15 @@ function App() {
         </section>
       </main>
 
-      <footer className="bg-[#262262] text-white mt-16 py-12">
+      <footer className="bg-[#f0f0f2] text-white mt-16 py-12">
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex items-center mb-6 md:mb-0">
-              <img src="/logo.svg" alt="Al Salam Logo" className="h-10 w-10 mr-3" />
-              <span className="text-xl font-semibold">Al Salam</span>
+              <img src="/LogoDeBase.png" alt="Al Salam Logo" className="h-25 w-40 mr-3" />
             </div>
             <div className="text-center md:text-right">
-              <p className="mb-2">© 2025 Al Salam Islamic Center Lörrach-Maulburg</p>
-              <p className="text-gray-300 text-sm">{t('footerText')}</p>
+              <p className="mb-2 text-[#262262]">© 2025 Al Salam Islamic Center Lörrach-Maulburg</p>
+              <p className="text-[#009245] text-sm">{t('footerText')}</p>
             </div>
           </div>
         </div>
